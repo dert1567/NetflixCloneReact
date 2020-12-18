463e407e3e36aca7066f64781c4df662319062ef
dert1567 <35528731+dert1567@users.noreply.github.com>
2020-12-18 16:38:41 +0100

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -8,13 +8,13 @@ import 'firebase/auth';
 
 // we need a config herer
 const config = {
-    apiKey: "AIzaSyBk07C8w61aVdzNm4EPSOIbEqo-0-LR4Vk",
-    authDomain: "netflix-1b680.firebaseapp.com",
-    databaseURL: "https://netflix-1b680.firebaseio.com",
-    projectId: "netflix-1b680",
-    storageBucket: "netflix-1b680.appspot.com",
-    messagingSenderId: "562880699250",
-    appId: "1:562880699250:web:25a851c2b8180227f8dbeb",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "´",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
 const firebase = Firebase.initializeApp(config);
